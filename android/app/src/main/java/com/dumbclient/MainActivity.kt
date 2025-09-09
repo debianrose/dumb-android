31502cec09b3a4e62a81befab68a3106ba11cef2
debianrose <speedwagoh@gmail.com>
2025-09-10 00:15:38 +0300

    Start Creating Client!

--- android/app/src/main/java/com/dumbclient/MainActivity.kt
+++ android/app/src/main/java/com/dumbclient/MainActivity.kt
@@ -2,21 +2,17 @@ package com.dumbclient
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+    override fun getMainComponentName(): String = "dumbclient"
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "dumbclient"
-
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    override fun createReactActivityDelegate(): ReactActivityDelegate {
+        return DefaultReactActivityDelegate(
+            this,
+            mainComponentName,
+            // If you opted-in for the New Architecture, we enable Fabric here.
+            BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        )
+    }
 }
